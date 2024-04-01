8e3a45ed38811e1bdfad55dbd3ca97563fdac088
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2024-04-01 18:00:10 -0400

    Start basic exam controller class

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+class ExamController:
+    def send_user_data(name, email, pass_traning, num_of_qustions_passed):
+        #Send user data to database here
+        print(name, email, pass_traning, num_of_qustions_passed)
+
+    def calculate_pass(num_of_questions_passed):
+        all_questions = 3
+
+        if all_questions == num_of_questions_passed:
+            return True
+
 def main():
     print("test")
 
