df7e92c2f5aab4bf37d57c00b077b93c4baf95ee
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2024-04-03 23:52:33 -0400

    Updated GUI
    Added Title, Name and Email Entry, and Button

--- main.py
+++ main.py
@@ -65,12 +65,51 @@ class questions:
     def all_questions(self):
         self._all_questions
 
-def main():
-    root = tk.Tk()
-    message = tk.Label(root, text="Y'all should really be working on this by now")
+#I made the App its own class, idk if we should keep it this way?
+class App:
+    #function for getting user name and email inputted by user
+    def enter_user_info(self):
+        print(self.nameEntry.get())
+        print(self.emailEntry.get())
+        # if input is valid, create new instance of user 
 
-    root.mainloop()
-    print("test")
+    def __init__(self):
+        #window
+        self.window = tk.Tk()
+        self.window.title("Training Software")
+        self.window.geometry("640x480")
+
+        # title
+        # need to center the contents
+        title_label = tk.Label(master=self.window, text="Training Software", font="Arial 24")
+        title_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        
+        # name input
+        nameLabel = tk.Label(master=self.window, text="Name: ", font="Arial 12")
+        nameLabel.grid(row=1, column=0, padx=5, pady=5)
+        self.nameEntry = tk.StringVar()
+        nameEntry = tk.Entry(master=self.window)
+        nameEntry.grid(row=1, column=1, padx=5, pady=5)
+
+        # email input
+        emailLabel = tk.Label(master=self.window, text="Email: ", font="Arial 12")
+        emailLabel.grid(row=2, column=0, padx=5, pady=5)
+        self.emailEntry = tk.StringVar()
+        emailEntry = tk.Entry(master=self.window, textvariable=self.emailEntry)
+        emailEntry.grid(row=2, column=1, padx=5, pady=5)
+
+        # Enter button
+        button = tk.Button(master=self.window, text="Enter", command=self.enter_user_info)
+        button.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+
+
+        #run 
+        self.window.mainloop()
+        print("App Closed")
+
+def main():
+    app = App()
+    
 
 if __name__ == "__main__":
     main()
