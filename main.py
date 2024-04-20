25dd834998e8ba85c65ef245d37d519980571cbe
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2024-04-20 13:13:50 -0400

    added 3rd question and end page

--- main.py
+++ main.py
@@ -85,13 +85,13 @@ class App(ExamController):
         self.main_frame = tk.Frame(self.window)
         self.main_frame.pack(fill=tk.BOTH, expand=True)
         
-        question_1_label = tk.Label(master=self.main_frame, text="Question 1", font="Arial 24")
+        question_1_label = tk.Label(master=self.main_frame, text="Q1: What is Social Engineering?", font="Arial 24")
         question_1_label.pack(padx=10, pady=10)
 
         info_text = """What is Social Engineering?"""
 
-        instructions_label = tk.Label(master=self.main_frame, text=info_text, font="Arial 12")
-        instructions_label.pack(padx=10, pady=10)
+        # instructions_label = tk.Label(master=self.main_frame, text=info_text, font="Arial 12")
+        # instructions_label.pack(padx=10, pady=10)
 
         radio_value = tk.StringVar(value="-1")
         checkbox1 = tk.Radiobutton(
@@ -147,6 +147,7 @@ class App(ExamController):
         email_t = tk.Text(master=self.main_frame, height=15, width=75)
         email_text = f"""
 To: {email}
+From: sellco_security_team@gmail.com
 Subject: Urgent: Action Required - Security Alert for Your Account
 
 Dear {name},
@@ -177,7 +178,7 @@ Security Team
         email_t.insert(tk.END, email_text)
         email_t['state'] = 'disabled'
 
-        question_2_label = tk.Label(master=self.main_frame, text="Analyze the Following Email", font="Arial 24")
+        question_2_label = tk.Label(master=self.main_frame, text="Q2: Which Actions are Appropriate for this Email?", font="Arial 16")
         question_2_label.pack(padx=10, pady=10)
 
         self.question_entry = tk.StringVar()
@@ -217,18 +218,108 @@ Security Team
         self.main_frame = tk.Frame(self.window)
         self.main_frame.pack(fill=tk.BOTH, expand=True)
 
-        question_3_label = tk.Label(master=self.main_frame, text="Question 3 goes here", font="Arial 24")
-        question_3_label.pack(padx=10, pady=10)
+        name = self.currName
+        email = self.currEmail
+
+        email_t = tk.Text(master=self.main_frame, height=15, width=75)
+        email_text = f"""
+To: {email}
+From: citybank@gmail.com
+Subject: Urgent: Action Required - Security Alert for Your Account
+
+Dear Valued Customer,
+
+We regret to inform you that our security system has detected a suspicious transaction on your account. It appears that a purchase of $500 was made from an unrecognized location using your credit card details.
+
+To ensure the security of your account and prevent any further unauthorized transactions, we require your immediate attention. Please review the details of the transaction below:
+
+    Transaction Amount: $500
+    Date of Transaction: 4/20/24
+    Merchant: XYZ Online Store
+    Transaction Location: Orlando
+    Transaction Reference Number: 12345
+
+If you did not authorize this purchase or believe it to be fraudulent, please take the following steps:
+
+Click on the link provided below to access our secure account verification page.
+veryifymyaccount.com
+
+Once on the verification page, enter your login credentials (username and password) to confirm your identity.
+
+After verifying your account, you will be prompted to review and dispute the unauthorized transaction.
+
+Failure to verify your account and dispute the transaction within the next 24 hours may result in additional unauthorized charges and potential financial loss.
+
+Please note that for security reasons, we have temporarily suspended your account access until the issue is resolved. We apologize for any inconvenience this may cause and appreciate your cooperation in resolving this matter promptly.
+
+If you have any questions or concerns, please contact our customer support team immediately at 845-921-2379 or reply to this email.
+
+Thank you for your attention to this urgent matter.
+
+Sincerely,
+CityBank Security Team
+                """
+
+        email_t.pack()
+        email_t.insert(tk.END, email_text)
+        email_t['state'] = 'disabled'
+
+        question_2_label = tk.Label(master=self.main_frame, text="Q3: Which Actions are Appropriate for this Email?", font="Arial 16")
+        question_2_label.pack(padx=10, pady=10)
 
         self.question_entry = tk.StringVar()
-        
-        question_input_frame = tk.Frame(self.main_frame)
-        question_input_frame.pack(padx=10, pady=10)
-        question_text_entry = tk.Entry(master=question_input_frame, textvariable=self.question_entry)
-        question_text_entry.pack(padx=10, pady=10)
 
-        question_button = tk.Button(master=self.main_frame, text="Enter", command=self.next_page_question)
-        question_button.pack(padx=5, pady=5)
+        self.checkbox_varA = tk.BooleanVar(value=False)
+        checkbox1 = tk.Checkbutton(master=self.main_frame, text="A. Verify the sender's email address", variable=self.checkbox_varA)
+        checkbox1.pack(padx=(100, 5), pady=5, anchor="w")
+
+        self.checkbox_varB = tk.BooleanVar(value=False)    
+        checkbox2 = tk.Checkbutton(master=self.main_frame, text="B. Click on the provided link and enter login credentials", variable=self.checkbox_varB)
+        checkbox2.pack(padx=(100, 5), pady=5, anchor="w")
+
+        self.checkbox_varC = tk.BooleanVar(value=False)    
+        checkbox3 = tk.Checkbutton(master=self.main_frame, text="C. Call the number provided in the email", variable=self.checkbox_varC)
+        checkbox3.pack(padx=(100, 5), pady=5, anchor="w")
+
+        self.checkbox_varD = tk.BooleanVar(value=False)    
+        checkbox4 = tk.Checkbutton(master=self.main_frame, text="D. Contact the bank directly", variable=self.checkbox_varD)
+        checkbox4.pack(padx=(100, 5), pady=5, anchor="w")
+
+        self.checkbox_varE = tk.BooleanVar(value=False)    
+        checkbox5 = tk.Checkbutton(master=self.main_frame, text="E. Dismiss the email as a mistake", variable=self.checkbox_varE)
+        checkbox5.pack(padx=(100, 5), pady=5, anchor="w")
+
+        question_button_blah = tk.Button(master=self.main_frame, text="Enter", command=lambda : self.next_page_question() if (
+            self.checkbox_varA.get() and 
+            not self.checkbox_varB.get() and
+            not self.checkbox_varC.get() and
+            self.checkbox_varD.get() and
+            not self.checkbox_varE.get()
+            ) 
+            else print("incorrect"))
+        
+        question_button_blah.pack(padx=5, pady=5)
+
+    def end_page(self):
+        # Create new frame for the next page
+        self.main_frame = tk.Frame(self.window)
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
+
+        info_text = """You have completed the Social Engineering Challenge! 
+       You tested your ability to identify and respond to various social engineering tactics 
+       and successfully navigated through different scenarios and make decisions 
+       that help you avoid falling victim to social engineering attacks."""
+
+        # Add widgets for the next page
+        info_page_label = tk.Label(master=self.main_frame, text="Congratulations!", font="Arial 24")
+        info_page_label.pack(padx=10, pady=10)
+
+        # Add more widgets as needed for the next page
+        instructions_label = tk.Label(master=self.main_frame, text=info_text, font="Arial 12")
+        instructions_label.pack(padx=10, pady=10)
+
+        # question_1_button = tk.Button(master=self.main_frame, text="Enter", command=self.next_page_question)
+        # question_1_button.pack(padx=5, pady=5)
 
     def next_page_question(self):
         # Destroy current frame
@@ -243,6 +334,9 @@ Security Team
             self.question_num += 1
         elif self.question_num == 3:
             self.question_3_page()
+            self.question_num += 1
+        elif self.question_num == 4:
+            self.end_page()
             self.question_num += 1
     
     def info_page(self):
